ba1708fcc05e3c2ede6298974afc035dcd91ea3a
Josh <joshbooks@gmail.com>
2019-09-22 13:13:03 -0700

    get schema definitions working

--- src/lib.rs
+++ src/lib.rs
@@ -17,13 +17,20 @@ mod tests {
     #[test]
     fn test_schema_serialization() {
         let version = semver::Version::parse("0.0.1").unwrap();
-        let sixtyFourBit = IntWidth::SixtyFour;
-        let uint64 = PainlessValue::PainlessInteger { sign : false, width : sixtyFourBit, int_value: 64 };
+        let sixty_four_bit = IntWidth::SixtyFour;
+        let uint64 = PainlessValue::PainlessInteger { sign : false, width : sixty_four_bit, int_value: 64 };
         let painless_var = PainlessVariable { name : "test_int".to_string(), painless: uint64 };
 
         let painless_struct = PainlessStruct { variables : Box::new([painless_var]) };
 
+        let painless_schema =
+            PainlessSchema 
+            { 
+                painless_schema_version : version,
+                painless_schema_name : "test name".to_string(),
+                painless_structs : Box::new([painless_struct]) 
+            };
 
-        println!("{}", serde_yaml::to_string(&painless_struct).unwrap());
+        println!("{}", serde_yaml::to_string(&painless_schema).unwrap());
     }
 }
--- src/schema_definition/mod.rs
+++ src/schema_definition/mod.rs
@@ -3,8 +3,9 @@ use serde::{Serialize, Deserialize};
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct PainlessSchema {
-    painless_schema_version: Version,
-    painless_structs: Box<[PainlessStruct]>
+    pub painless_schema_version: Version,
+    pub painless_schema_name: String,
+    pub painless_structs: Box<[PainlessStruct]>
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -24,8 +25,7 @@ pub enum PainlessValue {
     PainlessInteger { sign: bool, width: IntWidth, int_value: u64 },
     PainlessBoolean { bool_value: bool },
     PainlessList { elem_type: Box<PainlessValue>, size: u32, list_value: Box<[PainlessValue]> },
-    PainlessMap { keyValueList: Box<[(Box<str>, PainlessValue)]> },
-    PainlessObject {fields: Box<[PainlessVariable]>} // a PainlessObject is just like a PainlessMap, but without the type constraints
+    PainlessMap { key_value_list: Box<[(Box<str>, PainlessValue)]> },
 }
 
 #[derive(Serialize, Deserialize, Debug)]
